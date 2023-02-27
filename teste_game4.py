b94d5b496d42699193938d3fcaba72fe2ad65262
mullergc <mullergabriel94@gmail.com>
2023-02-27 14:02:40 -0300

    funcoes_text

--- teste_game4.py
+++ teste_game4.py
@@ -22,16 +22,19 @@ def introScene():
 
 def choices_gov(res):
     print("You have this amount to manage:" + str(res) + ".")
-    print("How much you will give to primary care? (0-100%)")
-        userInput = ""
-    print("How much you will give to Hospital? (0-100%)")
-        userInput2 = ""
-    print("How much you will give to Hospital? (0-100%)")
-    print("How much you will give to Hospital? (0-100%)")
-        userInput3 = ""
+    print("How much you will give to Primary care (first) Hospital (second) and Media ? (0-100%)")
+        x, y, z = input("Enter three values: ").split()
 
-primcare = res * userInput
-        hospital = res * userInput2
+        userInput = int(x)
+        userInput2 = int(y)
+        userInput3 = int(z)
+
+    if (userInput + userInput2 + userInput3) > 100:
+         print("Please enter a valid option.")
+    else:
+        primcare = int(res) * userInput
+        hospital = int(res) * userInput2
+        icu = int(res) * userInput3
 
 if __name__ == "__main__":
     while True:
